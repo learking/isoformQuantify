6ad179728def7cd4cb2640259116f746f492e9db
Kuangyu Wang <wangw1023@gmail.com>
2012-07-31 11:57:30 -0400

    gene lookup service ready

--- isoformQuantifyApp/static/isoformQuantifyApp.js
+++ isoformQuantifyApp/static/isoformQuantifyApp.js
@@ -8,32 +8,59 @@ var jsonp = function(url) {
     head.removeChild(script);
 };
 
+var showGeneID = function(geneInfo){
+    var elem = d3.select("#chart");
+    elem.select("#geneInfo").remove();
+    
+    var geneInfo_svg = elem
+	.append("svg")
+	.attr("id", "geneInfo")
+	.attr("width", canvas_width)
+	.attr("height", 60)
+	.append("text")
+	.attr("x", 0)
+	.attr("y", 40)
+	.attr("font-size", 40)
+	.text(geneInfo);
+};
+
 var getGeneHits = function(geneHits){
-// no hit
-
-// multiple hit
-
-// one hit, but not within dataset
-
+    
     numHits = Object.keys(geneHits).length;
+    // multiple hits
+    if (numHits > 1){
+	alert("Warning: search result is not unique");
+    }
+
     for (var mgi_id in geneHits){
 	gene_mgi_id = mgi_id;
 	break;
     }
-    console.log(gene_mgi_id);
-    console.log(mgi2ensembl[gene_mgi_id]);
 
-    var gene_ensembl_id = mgi2ensembl[gene_mgi_id];
+    // no hit
+    if(gene_mgi_id == "Not found"){
+	alert("Gene lookup service won't be able to locate this gene!");
+    }
+    else{
+	var geneInfo = "Gene Symbol:" + geneHits[gene_mgi_id]["symbol"] + " || " + "MGI ID:" + gene_mgi_id + " || " + "ENSEMBL ID:" +  mgi2ensembl[gene_mgi_id];
 
-    d3.json("gene_" + gene_ensembl_id + ".json", function(json) {
-	try{
+	showGeneID(geneInfo);
+
+	var gene_ensembl_id = mgi2ensembl[gene_mgi_id];
+
+	d3.json("gene_" + gene_ensembl_id + ".json", function(json) {
+	    try{
 		makeGeneGraph(json);
-		getIsoformAbundance(json);
-	}
-	catch(error){
-	    alert("this gene does not exist");
-	}
-    });  
+		if(json.transcripts.length > 1){
+		    getIsoformAbundance(json);
+		}
+	    }
+	    catch(error){
+		// one hit, but not within dataset
+		alert("this gene does not exist");
+	    }
+	});  
+    }
 
 };
 
